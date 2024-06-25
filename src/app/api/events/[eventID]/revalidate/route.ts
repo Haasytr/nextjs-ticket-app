02c07ed31187d45e91ca679110cbc4273740078e
Vinicius Oliveira <viniciuszero@Live.com>
2024-06-25 16:24:21 -0300

    finishing project

---
+++ src/app/api/events/[eventID]/revalidate/route.ts
@@ -0,0 +1,9 @@
+import { revalidateTag } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export function POST(request: NextRequest, { params }: { params: { eventID: string } }) {
+    revalidateTag("events");
+    revalidateTag(`events/${params.eventID}`)
+
+    return new Response(null, { status: 204 })
+}
